a1cdd9f281610301c7df1bd66264d00006480c52
Arth Limchiu <limchiuarth@gmail.com>
2018-08-18 22:58:33 +0800

    Added backstack handling for login and sign up fragments

--- app/src/main/java/com/imakeanapp/chatappkotlin/MainActivity.kt
+++ app/src/main/java/com/imakeanapp/chatappkotlin/MainActivity.kt
@@ -2,11 +2,83 @@ package com.imakeanapp.chatappkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.imakeanapp.chatappkotlin.auth.view.LoginFragment
+import com.imakeanapp.chatappkotlin.auth.view.SignUpFragment
+import com.imakeanapp.chatappkotlin.auth.view.WelcomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainActivityFragmentsListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showWelcomeFragment()
+    }
+
+    override fun onLoginClick() {
+        showLoginFragment()
+    }
+
+    override fun onSignUpClick() {
+        showSignUpFragment()
+    }
+
+    override fun onLogoutClick() {
+        showSignUpFragment()
+    }
+
+    override fun onLoginSuccess(username: String) {
+    }
+
+    override fun onSignUpSuccess(username: String) {
+    }
+
+    private fun showWelcomeFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragment_container, WelcomeFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+    private fun showLoginFragment() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(R.animator.slide_in_from_right, R.animator.slide_out_to_left,
+                    R.animator.slide_in_from_left, R.animator.fade_out)
+            replace(R.id.fragment_container, LoginFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+    private fun showSignUpFragment() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        }
+
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(R.animator.slide_in_from_right, R.animator.slide_out_to_left,
+                    R.animator.slide_in_from_left, R.animator.fade_out)
+            replace(R.id.fragment_container, SignUpFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.findFragmentByTag("MessagesFragment") != null) {
+            showSignUpFragment()
+            return
+        }
+
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        } else {
+            finish()
+        }
     }
 }
